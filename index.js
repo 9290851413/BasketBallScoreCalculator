284cae80b74693da20c05b367cc2e533a3c40e13
kiran siddani <svnkiran123@gmail.com>
2025-11-10 11:06:01 +0530

    Commentary Function - partial
    Commentary Function Implemented  partially, html and css are completed.

--- index.js
+++ index.js
@@ -11,8 +11,32 @@ function increment(team,points) {
     if(team == "home") {
         homePoints += points
         homeScore.textContent = homePoints
+        commentary()
     } else if(team == "guest") {
         guestPoints += points
         guestScore.textContent = guestPoints
+        commentary()
+    }
+}
+
+
+// function for the commentary section
+function commentary() {
+    // varibale to get and display the commentary
+    let commentary = document.getElementById("commentary")
+    if(homePoints == 1 || homePoints == 2 || homePoints == 3) {
+        commentary.textContent = "Home Team Scored Their First Points."
+    } 
+    else if(guestPoints == 1 || guestPoints == 2 || guestPoints == 3) {
+        commentary.textContent = "Guest Team Scored Their First Point."
+    }
+    if(homePoints == 11 || guestPoints == 11) {
+        commentary.textContent = "Half Of The Match Is Finished."
+    }
+    if(homePoints >= 21 && guestPoints < 21) {
+        commentary.textContent = "Home Team Won The Match"
+    }
+    if(guestPoints >= 21 && homePoints < 21) {
+        commentary.textContent = "Guest Team Won The Match"
     }
 }
